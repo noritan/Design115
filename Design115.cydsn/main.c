6454b6d1682cd8f497d99882b9fa773f9526cd8d
noritan <github@noritan.org>
2014-02-10 17:19:18 +0900

    Initial commit for PSoC Creator
    To start a project,
    Added components USBUART, LCD, SW2/3, LED3/4
    Added an initial program to copy SW status into LED

---
+++ Design115.cydsn/main.c
@@ -0,0 +1,27 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include <project.h>
+
+int main()
+{
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
+    for(;;)
+    {
+        /* Place your application code here. */
+        LED4_Write(SW2_Read());
+        LED3_Write(SW3_Read());
+    }
+}
+
+/* [] END OF FILE */
