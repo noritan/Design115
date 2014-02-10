bc41537949e407529469d984c680bcab713e8339
noritan <github@noritan.org>
2014-02-10 17:59:16 +0900

    Implemented echo back for USBUART
    Implemented
    Initialization for USB device
    Bus reset detection
    Echo back routine for UART

--- Design115.cydsn/main.c
+++ Design115.cydsn/main.c
@@ -11,16 +11,50 @@
 */
 #include <project.h>
 
+void echoBackUart(void) {
+	uint8   rxmessage[64];
+	uint8   length;
+    uint8   i;
+
+    if (USBUART_DataIsReady()) {                // PCからのデータ受信待ち
+        length = USBUART_GetCount();            // 受信したデータ長の取得
+        USBUART_GetData(rxmessage, length);     // 受信データを取得
+
+        for (i = 0; i < length; i++) {
+            LCD_PutChar(rxmessage[i]);		    // LCDに受信データを表示
+        }
+		while (!USBUART_CDCIsReady());	        //送信バッファが空か確認
+	    USBUART_PutData(rxmessage, length);		//確認用に受信したデータを送信
+	}
+}
+
 int main()
 {
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+	CyGlobalIntEnable;                          // 割り込みの有効化    
+	LCD_Start();                                // LCDの初期化
+    USBUART_Start(0, USBUART_5V_OPERATION);     // 動作電圧5VにてUSBFSコンポーネントを初期化
 
-    /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
-    for(;;)
-    {
-        /* Place your application code here. */
-        LED4_Write(SW2_Read());
-        LED3_Write(SW3_Read());
+    for (;;) {
+        // 初期化終了まで待機
+        while (USBUART_GetConfiguration() == 0);
+
+        USBUART_IsConfigurationChanged();       // CHANGEフラグを確実にクリアする
+
+        USBUART_CDC_Init();                     // USBFSのCDCコンポーネントを初期化
+
+        for (;;) {
+            // 設定が変更されたら、再初期化をおこなう
+            if (USBUART_IsConfigurationChanged()) {
+                break;
+            }
+            
+            // UARTのエコーバック処理
+            echoBackUart();
+
+            // SWをLEDにコピーする処理
+            LED4_Write(SW2_Read());
+            LED3_Write(SW3_Read());
+        }
     }
 }
 
