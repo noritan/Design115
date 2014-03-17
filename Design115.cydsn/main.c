05a39401a9a21e8f5283ac7d04e47445db01cb07
noritan <github@noritan.org>
2014-03-18 00:40:03 +0900

    Implemented SCSI commands
    Implemented REQUEST_SENSE, INQUIRY, READ CAPACITY (10)
    Added some functions to reentrant list

--- Design115.cydsn/main.c
+++ Design115.cydsn/main.c
@@ -199,10 +199,25 @@ void echoBackUart(void) {
 
 // MSCデバイスのステートマシン宣言
 typedef enum MscState_ {
-    MSCST_CBW_WAIT = 0,                         // CBWパケット待ち
+    MSCST_CBW_WAIT = 0,                 // CBWパケット待ち
+    MSCST_IN_WAIT,                      // BULK-IN送信待ち
+    MSCST_IN_SET,                       // BULK-INデータセット
+    MSCST_IN_COMPLETE,                  // BULK-IN送信完了
+    MSCST_CSW_PREPARE,                  // CSW準備
+    MSCST_CSW_WAIT,                     // CSW送信待ち
+    MSCST_CSW_SET,                      // CSWデータセット
+    MSCST_CSW_COMPLETE,                 // CSW送信完了
 }   MscState;
 
-MscState    mscState;                           // 状態変数
+MscState    mscState;                   // 状態変数
+
+// SCSIコマンド
+enum ScsiCommand {
+    SCSI_REQUEST_SENSE = 0x03,          // REQUEST SENSE
+    SCSI_INQUIRY = 0x12,                // INQUIRY
+    SCSI_READ_CAPACITY_10 = 0x25,       // READ CAPACITY(10)
+    SCSI_READ_10 = 0x28,                // READ(10)
+};
 
 // MSCデバイスの初期設定
 void mscInit(void) {
@@ -213,6 +228,26 @@ void mscInit(void) {
 
 uint16      mscCbwLength;                       // CBWの長さ
 uint8       cbw[31];                            // CBWの受信バッファ
+uint8       csw[13] = {'U', 'S', 'B', 'S'};     // CSWの送信バッファ
+uint32      mscCbwDataTransferLength;           // CBWのデータ転送長
+uint8       mscBufferIn[64];                    // Bulk-in転送のバッファ
+uint8       mscBufferInLength;                  // Bulk-inバッファ内のデータ長
+
+// フィールドから32ビットの値を取り出す
+uint32 mscGetValue32(uint8 *s) {
+    return ((((uint32)((((uint16)s[3])<<8)|s[2])<<8)|s[1])<<8)|s[0];
+}
+
+// フィールドに32ビットの値を書込む
+void mscPutValue32(uint8 *p, uint32 v) {
+    p[0] = v & 0x000000FF;
+    v >>= 8;
+    p[1] = v & 0x000000FF;
+    v >>= 8;
+    p[2] = v & 0x000000FF;
+    v >>= 8;
+    p[3] = v & 0x000000FF;
+}
 
 // CBWを表示する
 void mscShowCbw(void) {
@@ -225,6 +260,155 @@ void mscShowCbw(void) {
     }
 }
 
+// CBWは有効か?
+uint8 mscCbwIsValid(void) {
+    if (mscCbwLength != 31) return 0;
+    if (cbw[0] != 'U') return 0;        // Signature
+    if (cbw[1] != 'S') return 0;
+    if (cbw[2] != 'B') return 0;
+    if (cbw[3] != 'C') return 0;
+    if (cbw[12] & 0x7F) return 0;       // Flag[6:0]=0
+    if (cbw[13] != 0) return 0;         // LUN=0
+    // CBLength
+    if ((cbw[14] < 1)||(cbw[14] > 16)) return 0;
+    return 1;
+}
+
+static uint8 senseData[18];
+
+void mscScsiSenseDataInit(void) {
+    senseData[ 0] = 0x00;               // RESPONSE CODE
+    senseData[ 1] = 0x00;               // Obsoleted
+    senseData[ 2] = 0x00;
+    senseData[ 3] = 0x00;
+    senseData[ 4] = 0x00;
+    senseData[ 5] = 0x00;
+    senseData[ 6] = 0x00;
+    senseData[ 7] = 0x00;
+    senseData[ 8] = 0x00;
+    senseData[ 9] = 0x00;
+    senseData[10] = 0x00;
+    senseData[11] = 0x00;
+    senseData[12] = 0x00;
+    senseData[13] = 0x00;
+    senseData[14] = 0x00;
+    senseData[15] = 0x00;
+    senseData[16] = 0x00;
+    senseData[17] = 0x00;
+}
+
+static uint8 const CYCODE inquiryData[36u] = {
+    0x00,               // PERIPHERAL
+    0x80,               // RMB = removable
+    0x00,               // Version: No conformance claim to standard
+    0x02,               // NORMACA=0, HISUP=0, FROMAT=2
+    0x20,               // ADDITIONAL 32 Bytes
+    0x80,               // SCCS = 1: Storage Controller Component
+    0x00,
+    0x00,
+    'n', 'o', 'r', 'i', 't', 'a', 'n', ' ',     // Vender
+    'P', 'S', 'o', 'C', ' ', 'M', 'S', 'C',     // Product
+    ' ', 'D', 'r', 'i', 'v', 'e', ' ', ' ',
+    '1', '.', '0', '0'                          // Revision
+};
+
+// INQUIRYコマンド応答
+uint8 mscScsiInquiryPrepare(void) {
+    uint8       i;
+    
+    mscScsiSenseDataInit();
+    mscBufferInLength = sizeof inquiryData;
+    for (i = 0; i < sizeof inquiryData; i++) {
+        mscBufferIn[i] = inquiryData[i];
+    }
+    if (mscCbwDataTransferLength > mscBufferInLength) {
+        csw[12] = 1;
+    }
+    mscState = MSCST_IN_WAIT;
+    return 1;
+}
+
+// 未対応命令へのエラー応答
+void mscScsiInvalidCommand(void) {
+    senseData[ 0] = 0x70;               // Response Code = Current error
+    senseData[ 2] = 0x05;               // SenseKey = ILLEGAL REQUEST
+    senseData[12] = 0x20;               // Additional Sense Code = INVALID COMMAND
+}
+
+// REQUEST SENSEコマンド応答
+uint8 mscScsiRequestSensePrepare(void) {
+    uint8       i;
+    
+    mscBufferInLength = sizeof senseData;
+    for (i = 0; i < sizeof senseData; i++) {
+        mscBufferIn[i] = senseData[i];
+    }
+    mscState = MSCST_IN_WAIT;
+    return 1;
+}
+
+// CAPACITY 256Byte*1024Block=256kByte
+static uint8 const CYCODE capacityData[8] = {
+    0x00, 0x00, 0x03, 0xFF,             // 1024Blocks
+    0x00, 0x00, 0x01, 0x00,             // 256Bytes/Block
+};
+
+// CAPACITYデータの準備
+uint8 mscScsiReadCapacityPrepare(void) {
+    uint8       i;
+    
+    mscScsiSenseDataInit();
+    mscBufferInLength = sizeof capacityData;
+    for (i = 0; i < sizeof capacityData; i++) {
+        mscBufferIn[i] = capacityData[i];
+    }
+    if (mscCbwDataTransferLength > mscBufferInLength) {
+        csw[12] = 1;
+    }
+    mscState = MSCST_IN_WAIT;
+    return 1;    
+}
+
+// 未対応IN命令応答
+void mscScsiUnknownIn(void) {
+    mscScsiInvalidCommand();
+    mscBufferInLength = 0;              // Nullパケット
+    csw[12] = 1;                        // Command failed
+    mscState = MSCST_IN_WAIT;
+}
+
+// 未対応OUT命令応答
+void mscScsiUnknownOut(void) {
+}
+
+// コマンド解析
+void mscCbwParse(void) {
+    // データ転送長を保存
+    mscCbwDataTransferLength = mscGetValue32(&cbw[8]);
+    putstr("\nDataTransferLength=");
+    putdec32(mscCbwDataTransferLength, 0);
+    
+    // コマンドによる処理分岐
+    switch (cbw[15]) {
+        case SCSI_INQUIRY:
+            if (mscScsiInquiryPrepare()) return;
+            break;
+        case SCSI_REQUEST_SENSE:
+            if (mscScsiRequestSensePrepare()) return;
+            break;
+        case SCSI_READ_CAPACITY_10:
+            if (mscScsiReadCapacityPrepare()) return;
+            break;
+        default:
+            if (cbw[12]) {
+                mscScsiUnknownIn();
+            } else {
+                mscScsiUnknownOut();
+            }
+            break;
+    }
+}
+
 // CBW待ち状態の処理
 void mscCbwWait(void) {
     if (USBUART_GetEPState(MSC_OUT) & USBUART_OUT_BUFFER_FULL) {
@@ -234,8 +418,53 @@ void mscCbwWait(void) {
         // CBWデータを読み込む
         USBUART_ReadOutEP(MSC_OUT, &cbw[0], mscCbwLength);
 
+        // CSWのタグを設定する
+        csw[4] = cbw[4];
+        csw[5] = cbw[5];
+        csw[6] = cbw[6];
+        csw[7] = cbw[7];
+        
+        // CSWの初期値を設定する
+        csw[12] = 0;
+
         // CBWデータを表示する
         mscShowCbw();
+        
+        // コマンドの解釈
+        if (mscCbwIsValid()) {          // コマンドとして受け入れられるか？
+            mscCbwParse();              // コマンド解析
+        }
+    }
+}
+
+// ホストへのデータ送信待ち
+void mscInWait(void) {
+    if (USBUART_GetEPState(MSC_IN) & USBUART_IN_BUFFER_EMPTY) {
+        putstr("\nSend BULK-IN : ");
+        putdec16(mscBufferInLength, 0);
+        // バッファを送信する
+        USBUART_LoadInEP(MSC_IN, &mscBufferIn[0], mscBufferInLength);
+        mscCbwDataTransferLength -= mscBufferInLength;
+        if (csw[12] || (mscCbwDataTransferLength == 0)) {
+            mscState = MSCST_CSW_PREPARE;
+        }
+    }
+}
+
+// CSWデータの準備
+void mscCswPrepare(void) {
+    mscPutValue32(&csw[8], mscCbwDataTransferLength);
+    mscState = MSCST_CSW_WAIT;
+}
+
+// CSWデータの送信
+void mscCswWait(void) {
+    if (USBUART_GetEPState(MSC_IN) & USBUART_IN_BUFFER_EMPTY) {
+        putstr("\nSend CSW : ");
+        puthex8(csw[12]);
+        // バッファを送信する
+        USBUART_LoadInEP(MSC_IN, &csw[0], sizeof csw);
+        mscState = MSCST_CBW_WAIT;
     }
 }
 
@@ -244,6 +473,15 @@ void mscDispatch(void) {
     switch (mscState) {
         case MSCST_CBW_WAIT:
             mscCbwWait();
+            break;
+        case MSCST_IN_WAIT:
+            mscInWait();
+            break;
+        case MSCST_CSW_PREPARE:
+            mscCswPrepare();
+            break;
+        case MSCST_CSW_WAIT:
+            mscCswWait();
             break;
     }
 }
